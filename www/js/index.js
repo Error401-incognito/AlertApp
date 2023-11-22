f98ff56e600d204ff188fdf3023fe2159ec97d4c
Error401-incognito <shankymicrocosm@gmail.com>
2023-11-22 23:53:15 +0530

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+// www/js/index.js
+
+function showAlert() {
+    navigator.notification.alert(
+        'This is a simple alert message!',  // message
+        alertDismissed,                     // callback
+        'Alert',                            // title
+        'OK'                                // buttonName
+    );
+}
+
+function alertDismissed() {
+    console.log('Alert dismissed');
+}
